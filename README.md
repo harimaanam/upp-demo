7295d4c56eb3252c74cc074da3bb41be8551fa21
harimaanam <harikrishna.maanam55@gmail.com>
2023-07-10 18:02:49 +0530

    upp-demo changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # upp-demo
+upp demo
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { FaSearch, FaBell, FaUser } from "react-icons/fa";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <span className="navbar-brand">SupplierCentral</span>
+        <span className="navbar-brand">XYZ Corporation</span>
+      </div>
+
+      <div className="navbar-icons">
+        <FaSearch className="icon" />
+        <FaBell className="icon" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+          />
+        </svg>
+
+        <FaUser className="icon" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
